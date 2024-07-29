8fa057e5007457df2e2759a11ce1be819cd14483
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-07-29 21:11:46 +0100

    first commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import dinning from "../assets/dining.jpg";
+import { LoginFormValues } from "../interfaces";
+
+const Login: React.FC = () => {
+  const formik = useFormik<LoginFormValues>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string().required("Required").min(8).max(16),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div
+      className="relative flex items-center justify-center min-h-screen"
+      style={{
+        backgroundImage: `url(${dinning})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="inset-0 absolute bg-black opacity-90 h-screen"></div>
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white p-6 rounded shadow-md w-full max-w-sm absolute"
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            className={`mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              formik.touched.email && formik.errors.email
+                ? "border-red-500"
+                : ""
+            }`}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-red-500 text-sm">{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            className={`mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              formik.touched.password && formik.errors.password
+                ? "border-red-500"
+                : ""
+            }`}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-red-500 text-sm">{formik.errors.password}</div>
+          ) : null}
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 "
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
